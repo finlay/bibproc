ba39e484a6b06c55fb87b31ea5c8424c351e79c4
Finlay Thompson <finlay@dragonfly.co.nz>
2013-10-02 14:55:30 +1300

    Updated bib2html in the same way as bib2sort

--- bib2html.hs
+++ bib2html.hs
@@ -1,9 +1,5 @@
 import System.Environment ( getArgs )
-import System.IO ( Handle, hPutStr, hClose, openTempFile, 
-                   hFlush, hSetEncoding, stdout, utf8 )
-import Control.Exception ( finally )
-import System.FilePath ()
-import System.Directory ( getTemporaryDirectory, removeFile )
+import System.IO ( hSetEncoding, stdout, utf8 )
 
 import Text.CSL
 import Text.Pandoc
@@ -20,7 +16,8 @@ main = do
     style        <- readCSLFile citefile
 
     -- Get references from standard in
-    refs <- withTempFile "bibfile.bib" getReferences
+    bibs <- getContents
+    refs <- readBiblioString Bibtex bibs
 
     let results = processBibliography procOpts style refs
     let wrapPandoc :: Block -> Pandoc
@@ -30,18 +27,3 @@ main = do
     hSetEncoding stdout utf8
     putStr . unlines . map (toHtml . (renderPandoc' style)) $ results
 
-getReferences :: FilePath -> Handle -> IO [Reference]
-getReferences bibfile bibhandle = do
-    bibs <- getContents 
-    hSetEncoding bibhandle utf8
-    hPutStr bibhandle bibs
-    hFlush bibhandle
-    readBiblioFile bibfile
-
-withTempFile :: String -> (FilePath -> Handle -> IO a) -> IO a
-withTempFile pattern func =
-    do tempdir <- catch (getTemporaryDirectory) (const $ return ".")
-       (tempfile, temph) <- openTempFile tempdir pattern 
-
-       finally (func tempfile temph) 
-               (hClose temph >> removeFile tempfile)
